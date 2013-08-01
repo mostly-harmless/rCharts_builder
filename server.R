74dd1dabd9e13256e34e6110c3932bf600408afa
timelyportfolio <kent.russell@timelyportfolio.com>
2013-08-01 10:21:08 -0500

    add x, y, chartType, and axis types
    still ugly but decent proof of concept

--- server.R
+++ server.R
@@ -4,15 +4,39 @@ require(data.table)
 
 shinyServer(function(input, output, session){
   mydata <- reactive({
-    switch(input$dataset, mtcars = mtcars, iris = iris,
+    switch(input$dataset, haireye = data.frame(HairEyeColor),mtcars = mtcars, iris = iris,
            faithful = data.table(faithful)
     )
   })
-  output$mychart <- renderChart2({
-    plotdata <- subset(data.frame(HairEyeColor),Sex=="Female")
-    d1 <- dPlot(Freq~Hair, groups = "Eye", data = plotdata, type="bar")
-    d1$yAxis( type = "addPctAxis" )
-    d1$legend(x = 60, y = 10, width = 700, height = 20, horizontalAlign = "right")
-    d1
+  
+  output$variableControls <- renderUI({
+    variables <- colnames(mydata())
+    tagList(
+      checkboxGroupInput("x", "Select X", variables),
+      checkboxGroupInput("y", "Select Y", variables)  
+    )
   })
+  
+  output$mychart <- renderChart2({
+    if("x" %in% names(input) && "y" %in% names(input)) {
+      if (!(is.null(input$x)) && !(is.null(input$y))) {
+        plotdata <- mydata()
+        d1 <- dPlot(
+          y=input$y,
+          x=input$x,
+          data = plotdata,
+          type=input$chartType
+        )
+        if("xType" %in% names(input) && !is.null(input$xType)) {
+          d1$xAxis(type = input$xType)
+        }
+        if("yType" %in% names(input) && !is.null(input$yType)) {
+          d1$yAxis(type = input$yType)
+        }
+        d1$legend(x = 60, y = 10, width = 700, height = 20, horizontalAlign = "right")
+        d1
+      }
+    }
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -4,11 +4,15 @@ shinyUI(pageWithSidebar(
   headerPanel('DataTables in Shiny with rCharts'),
   sidebarPanel(
     selectInput('dataset', 'Choose DataSet',
-                c('mtcars', 'iris', 'faithful')            
+                c('haireye', 'mtcars', 'iris', 'faithful')            
     ),
-    selectInput('pagination', 'Choose Pagination',
-                c('two_button', 'full_numbers')            
-    )
+    selectInput('chartType', 'Type of Chart',
+                c('line','bubble','bar')),
+    selectInput('xType', 'Type of x Axis',
+                c('addCategoryAxis','addMeasureAxis')),
+    selectInput('yType', 'Type of y Axis',
+                c('addMeasureAxis','addCategoryAxis')),
+    uiOutput('variableControls')           
   ),
   mainPanel(
     chartOutput('mychart', 'dimple') 
